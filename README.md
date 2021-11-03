3ff8e6b32682f39f09898d42abdeffb9ba47f66e
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-11-03 13:36:28 -0700

    complete business logic draft 1
    Co-Authored-by: Aaron Minnick <abminnick@gmail.com>
    Co-Authored-by: Karen Axon <kaxonwa@gmail.com>'

--- README.md
+++ README.md
@@ -17,5 +17,19 @@ Code:
       space2.addPairToCheck(space1,space3);
       console.log(space2.pairsToCheck[0][0]);
 Expected Output: "Object"
+
+Describe: Board()
+Test: It should return an object with nine spaces, an array that includes the nine spaces and an x as the mark.
+Code:
+      let board1 = new Board();
+      console.log(board1.spaces[4], board1.nextMark);
+Expected Output: "Object" 'x'
+
+Test: It should run addPairToCheck for every edge space that has a neighbor and the center space (four).
+Code:
+      let board1 = new Board();
+      console.log(board1.spaces[4].pairsToCheck);
+Expected Output: [[], [], [], []]
+      
+
 ```
-Describe:
--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,15 @@ function Board() {
   let seven = new Space();
   let eight = new Space();
   this.spaces = [zero, one, two, three, four, five, six, seven, eight];
+  one.addPairToCheck(zero, two);
+  three.addPairToCheck(zero, six);
+  four.addPairToCheck(zero, eight);
+  four.addPairToCheck(one, seven);
+  four.addPairToCheck(three, five);
+  four.addPairToCheck(two, six);
+  five.addPairToCheck(two, eight);  
+  seven.addPairToCheck(six, eight);
 
-  
   this.nextMark = 'x';
 };
 
@@ -23,10 +30,38 @@ Space.prototype.addPairToCheck = function(space1, space2) {
   this.pairsToCheck.push([space1, space2]);
 };
 
+Board.prototype.checkSpace = function(number) {
+  return spaces[number].value;
+};
 
-////////Tests//////////
-// let space1 = new Space();
-// let space2 = new Space();
-// let space3 = new Space();
-space2.addPairToCheck(space1,space3);
-console.log(space2.pairsToCheck[0][0]);
+Board.prototype.updateSpace = function(number) {
+  if (this.checkSpace(number) !== "") {
+    return false;
+  }
+  spaces[number].value = this.nextMark;
+  this.switchMark();
+};
+
+Board.prototype.switchMark = function() {
+  if (this.nextMark === 'x') {
+    this.nextMark = 'o';
+  } else {
+    this.nextMark = 'x'
+  }
+};
+
+Board.prototype.checkForWin = function() {
+  for(let i of this.spaces){
+    if ((i.value != '') && (i.pairsToCheck.length > 0)){
+      for(let pair of i.pairsToCheck){
+        if ((i.value===pair[0].value)&&(i.value===pair[1].value)){
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+let board1 = new Board();
+console.log(board1.spaces[4].pairsToCheck);
